73099b84596483cd979e0c8f556f4c9000b0d671
nagyl1999 <asd@asd.asd>
2023-03-20 18:08:51 +0100

    Train Logger Implementation added

---
+++ train-logger/src/main/java/hu/bme/mit/train/logger/TrainLoggerImp.java
@@ -0,0 +1,30 @@
+package hu.bme.mit.train.logger;
+
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.interfaces.TrainController;
+
+import com.google.common.collect.TreeBasedTable;
+import com.google.common.collect.Table;
+import java.util.Date;
+
+
+public class TrainLoggerImp {
+    private Table<Date, Integer, Integer> data;
+    private TrainUser user;
+    private TrainController controller;
+
+    public TrainLoggerImp(TrainUser user, TrainController controller) {
+        data = TreeBasedTable.create();
+        this.user = user;
+        this.controller = controller;
+    }
+
+    public void log() {
+        data.put(
+            new Date(),
+            user.getJoystickPosition(),
+            controller.getReferenceSpeed()
+        );
+    }
+
+}
---
+++ train-logger/src/test/java/hu/bme/mit/train/logger/TrainLoggerTest.java
@@ -0,0 +1,22 @@
+package hu.bme.mit.train.logger;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+public class TrainLoggerTest {
+
+    @Before
+    public void before() {
+        
+    }
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+
+    }
+}
