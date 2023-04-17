dcc20b83a79cd6f1694b9a59426a3b989bac8a77
Viktor <viktor@citomail.hu>
2023-04-17 17:14:12 +0200

    Controller thread sleep changed to 1000ms

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -17,7 +17,7 @@ public class TrainControllerImpl implements TrainController {
 		while(true){
 			followSpeed();
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
